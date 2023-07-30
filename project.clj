40ff47b6a13cfe96f5b0b13749396bf0fc947dc8
pilottam <tampailok@gmail.com>
2023-07-30 22:38:31 +0800

    fix

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojask-demo "0.1.0-SNAPSHOT"
   :description "a demo for clojask"
-  :url "https://github.com/PilotTam/clojask-demo"
+  :url "https://github.com/pilottam/clojask-demo"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :repl-options {:init (do (require '[clojure.core :refer :all :exclude [random-uuid]])
